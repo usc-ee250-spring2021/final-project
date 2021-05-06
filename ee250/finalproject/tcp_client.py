14f3e845f9a7b40c0874d8b9fa811bfce3fabbfa
Adrien Dor <ador@usc.edu>
2021-05-06 14:37:36 -0700

    Create tcp_client.py

---
+++ ee250/finalproject/tcp_client.py
@@ -0,0 +1,30 @@
+"""
+Server receiver buffer is char[256]
+If correct, the server will send a message back to you saying "I got your message"
+Write your socket client code here in python
+Establish a socket connection -> send a short message -> get a message back -> ternimate
+use python "input->" function, enter a line of a few letters, such as "abcd"
+"""
+import socket
+
+def main():
+    
+    # TODO: Create a socket and connect it to the server at the designated IP and port
+ HOST = '165.227.201.206'  # The server's hostname or IP address
+ PORT = 5530      # The port used by the server
+
+ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('', PORT))
+
+    
+    # TODO: Get user input and send it to the server using your TCP socket
+    user_input = input("Message to send to server: ")
+    s.sendall(user_input.encode())
+    # TODO: Receive a response from the server and close the TCP connection
+    data = s.recv(1024)
+    print(repr(data.decode()))
+    s.close()
+    
+
+if __name__ == '__main__':
+    main()
