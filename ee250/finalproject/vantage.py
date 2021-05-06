884dd481d250f3f4b464ccd269351e9e10bfd08d
Adrien Dor <ador@usc.edu>
2021-05-05 19:05:49 -0700

    Update vantage.py

--- ee250/finalproject/vantage.py
+++ ee250/finalproject/vantage.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 ts = TimeSeries(key=api_key, output_format='json')
 data = ts.get_intraday(symbol='AAPL',interval='1min', outputsize='full')
-print(data)
-#data['close'].plot()
-#plt.title('Intraday Times Series for the AAPL stock (1 min)')
-#plt.show()
+#print(data)
+data['4. close'].plot()
+plt.title('Intraday Times Series for the AAPL stock (1 min)')
+plt.show()
