0f343cae19476ff9811bb841b54b3d95ce82e4f1
Adrien Dor <ador@usc.edu>
2021-05-06 15:04:28 -0700

    Update tcp_client.py

--- ee250/finalproject/tcp_client.py
+++ ee250/finalproject/tcp_client.py
@@ -10,7 +10,7 @@ import socket
 def main():
     
     # TODO: Create a socket and connect it to the server at the designated IP and port
- HOST = '20.64.240.125'  # The server's hostname or IP address
+ HOST = '127.0.0.1'  # The server's hostname or IP address
  PORT = 5530      # The port used by the server
 
  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
