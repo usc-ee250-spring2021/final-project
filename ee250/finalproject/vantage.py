0072f6a27d3e947e77d2579ff451e431fe3bfd53
Adrien Dor <ador@usc.edu>
2021-05-06 15:57:37 -0700

    Update vantage.py

--- ee250/finalproject/vantage.py
+++ ee250/finalproject/vantage.py
@@ -11,6 +11,14 @@ Establish a socket connection -> send a short message -> get a message back -> t
 use python "input->" function, enter a line of a few letters, such as "abcd"
 """
 
+api_key = '2CGM6KNUEZ0KQUSL'  # AlphaVantage API Key
+
+#from .alphavantage import AlphaVantage as av
+
+
+
+
+
 
 def main():
     
@@ -23,7 +31,7 @@ def main():
 
     
     # TODO: Get user input and send it to the server using your TCP socket
-    user_input = input("Message to send to server: ")
+    user_input = input("Name of stock (ie AAPL, MSFT, BTC): ")
     s.sendall(user_input.encode())
     # TODO: Receive a response from the server and close the TCP connection
     response = s.recv(1024)
@@ -37,13 +45,9 @@ if __name__ == '__main__':
 # OpenWeatherMap API: https://openweathermap.org/current
 
 # TODO: Sign up for an API key
-api_key = '2CGM6KNUEZ0KQUSL'  # AlphaVantage API Key
-
-#from .alphavantage import AlphaVantage as av
-
 
 ts = TimeSeries(key=api_key, output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='AAPL',interval='1min', outputsize='full')
+data, meta_data = ts.get_intraday(symbol=user_input,interval='1min', outputsize='full')
 #print(data[0])
 data['4. close'].plot()
 plt.title('Intraday Times Series for the AAPL stock (1 min)')
