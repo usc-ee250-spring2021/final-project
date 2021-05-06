cc88f415e9983cfa2775f012b18720374fc26957
Adrien Dor <ador@usc.edu>
2021-05-05 18:42:44 -0700

    Create vantage.py

---
+++ ee250/finalproject/vantage.py
@@ -0,0 +1,17 @@
+import requests
+
+# OpenWeatherMap API: https://openweathermap.org/current
+
+# TODO: Sign up for an API key
+api_key = '2CGM6KNUEZ0KQUSL'  # AlphaVantage API Key
+
+from .alphavantage import AlphaVantage as av
+
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='AAPL',interval='1min', outputsize='full')
+data['close'].plot()
+plt.title('Intraday Times Series for the AAPL stock (1 min)')
+plt.show()
