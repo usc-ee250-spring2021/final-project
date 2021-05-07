a98ceaa52e8478e1d179242fde79f43b2c856523
Adrien Dor <ador@usc.edu>
2021-05-06 17:26:47 -0700

    Update vantage.py

--- ee250/finalproject/vantage.py
+++ ee250/finalproject/vantage.py
@@ -41,7 +41,7 @@ def main():
     plt.title('Intraday Times Series for the ' + user_input +' stock (1 min)')
     plt.show()
     # TODO: Receive a response from the server and close the TCP connection
-     mkt = market.to_string()
+    mkt = market.to_string()
     s.sendall(mkt.encode())
     response = s.recv(1024)
     print(repr(response.decode()))
